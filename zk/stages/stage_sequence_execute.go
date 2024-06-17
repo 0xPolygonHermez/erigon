a1550909b4544d3ada19f55800bedef4fdb3b2af
Valentin Staykov <v.staykov@razorlabs.com>
2024-06-17 11:34:43 +0000

    removed repeating block 1 to datastream

--- zk/stages/stage_sequence_execute.go
+++ zk/stages/stage_sequence_execute.go
@@ -19,7 +19,6 @@ import (
 	"github.com/ledgerwatch/erigon/eth/stagedsync"
 	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
 	"github.com/ledgerwatch/erigon/zk"
-	"github.com/ledgerwatch/erigon/zk/datastream/server"
 	zktx "github.com/ledgerwatch/erigon/zk/tx"
 	"github.com/ledgerwatch/erigon/zk/utils"
 )
@@ -84,11 +83,6 @@ func SpawnSequencingStage(
 
 		err = processInjectedInitialBatch(ctx, cfg, s, sdb, forkId, header, parentBlock, &blockContext)
 		if err != nil {
-			return err
-		}
-
-		srv := server.NewDataStreamServer(cfg.stream, cfg.chainConfig.ChainID.Uint64())
-		if err = server.WriteBlocksToStream(tx, sdb.hermezDb.HermezDbReader, srv, cfg.stream, 1, 1, logPrefix); err != nil {
 			return err
 		}
 
