3244891e577cd39c5e2d0633325ecf5a322cf13f
Valentin Staykov <v.staykov@razorlabs.com>
2024-06-25 12:38:17 +0000

    added the test smart contract

---
+++ zk/debug_tools/modx-contract/scripts/counter.js
@@ -0,0 +1,27 @@
+async function main() {
+try {
+   // Get the ContractFactory of your SimpleContract
+   const CounterContract = await hre.ethers.getContractFactory("Counter");
+
+   // Deploy the contract
+   const contract = await CounterContract.deploy();
+   // Wait for the deployment transaction to be mined
+   const deployResult = await contract.waitForDeployment();
+
+   console.log(deployResult)
+   console.log(`Counter contract deployed to: ${await contract.getAddress()}`);
+
+   const result = await contract.increment();
+   console.log(result);
+ } catch (error) {
+   console.error(error);
+   process.exit(1);
+ }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
