33dc06184fe095f57b5cf6cd36df3b8e4e31b396
Valentin Staykov <v.staykov@razorlabs.com>
2024-07-04 14:46:12 +0000

    revert test comments

--- zk/stages/stage_sequence_execute.go
+++ zk/stages/stage_sequence_execute.go
@@ -143,13 +143,13 @@ func SpawnSequencingStage(
 
 	var intermediateUsedCounters *vm.Counters
 	if !isLastBatchFinished {
-		intermediateCountersMap, _, err := sdb.hermezDb.GetBatchCounters(lastBatch)
+		intermediateCountersMap, found, err := sdb.hermezDb.GetBatchCounters(lastBatch)
 		if err != nil {
 			return err
 		}
-		// if !found {
-		// 	return fmt.Errorf("intermediate counters not found for batch %d", lastBatch)
-		// }
+		if !found {
+			return fmt.Errorf("intermediate counters not found for batch %d", lastBatch)
+		}
 
 		intermediateUsedCounters = vm.NewCountersFromUsedMap(intermediateCountersMap)
 	}
