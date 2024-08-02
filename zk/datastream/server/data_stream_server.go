fb3d16788bb1176e3f9319d1e96ace2a39a12ca4
Kamen Stoykov <k.stoykov@razorlabs.com>
2024-08-02 09:01:21 +0000

    restoring blockEnd in DS

--- zk/datastream/server/data_stream_server.go
+++ zk/datastream/server/data_stream_server.go
@@ -227,8 +227,7 @@ func createFullBlockStreamEntriesProto(
 	batchNumber uint64,
 	l1InfoTreeMinTimestamps map[uint64]uint64,
 ) (*DataStreamEntries, error) {
-	// entries := NewDataStreamEntries(len(filteredTransactions) + 3) // block bookmark + block + block end
-	entries := NewDataStreamEntries(len(filteredTransactions) + 2) // block bookmark + block + block end
+	entries := NewDataStreamEntries(len(filteredTransactions) + 3) // block bookmark + block + block end
 	blockNum := block.NumberU64()
 	// L2 BLOCK BOOKMARK
 	entries.Add(newL2BlockBookmarkEntryProto(blockNum))
@@ -275,7 +274,7 @@ func createFullBlockStreamEntriesProto(
 		entries.Add(transaction)
 	}
 
-	// entries.Add(newL2BlockEndProto(blockNum))
+	entries.Add(newL2BlockEndProto(blockNum))
 
 	return entries, nil
 }
