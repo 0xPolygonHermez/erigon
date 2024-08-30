65d31b725f5d01b816ab889c4f1db164a32678c9
Alonso <ARR551@protonmail.com>
2024-08-30 14:35:55 +0200

    error lint

--- zk/hermez_db/errors.go
+++ zk/hermez_db/errors.go
@@ -2,5 +2,7 @@ package hermez_db
 
 import "errors"
 
-var ErrorNotStored = errors.New("not stored")
-var ErrorL2BlockNumberNotMatch = errors.New("l2BlockNumber doesn't match")
+var (
+	ErrorNotStored             = errors.New("not stored")
+	ErrorL2BlockNumberNotMatch = errors.New("l2BlockNumber doesn't match")
+)
